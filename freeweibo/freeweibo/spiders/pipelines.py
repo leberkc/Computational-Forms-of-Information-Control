e4c1eb76b1b40b8449ce987bf9ccf8ef2d9e7d1e
ortizjhony <86069583+ortizjhony@users.noreply.github.com>
2021-07-03 09:40:26 -0400

    Update pipelines.py
    pipelines.py send the items stored in items.py to the mysql databse. Currently working on solving the current error:
    mysql.connector.errors.ProgrammingError: Failed processing format-parameters; Python 'list' cannot be converted to a MySQL type
    
    Lines 41 and 42. Those two variables are lists containing multiple possible hashtags

--- freeweibo/freeweibo/spiders/pipelines.py
+++ freeweibo/freeweibo/spiders/pipelines.py
@@ -6,8 +6,43 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import mysql.connector
 
 
-class FreeweiboPipeline:
+class FreeweiboPipeline(object):
+    def __init__(self):
+        self.create_connection()
+
+    def create_connection(self):
+        self.conn = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            passwd = 'Itagui22388!',
+            database = 'FreeWeiboPosts'
+            )
+        self.curr = self.conn.cursor()
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+
+    def store_db(self, item):
+        values =[
+            item['username'], 
+            item['postid'], 
+            item['repostscount'], 
+            item['censored'], 
+            item['deleted'], 
+            item['contains_adult_keyword'], 
+            item['contains_censored_keyword'], 
+            item['time_created'], 
+            item['freeweiboOGpostlink'], 
+            item['content'], 
+            item['hashtags'][:], 
+            item['hashtagsurls'][:], 
+            item['timestampscrapped']
+        ]
+        self.curr.execute(
+            "INSERT INTO Posts VALUES (NULL,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",values
+            )
+        self.conn.commit()
