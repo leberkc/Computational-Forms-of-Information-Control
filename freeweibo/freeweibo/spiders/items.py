729e1f0f0db016ce332762e6fc13560345c3f400
ortizjhony <86069583+ortizjhony@users.noreply.github.com>
2021-07-12 12:06:33 -0400

    Update items.py
    Update to items to be sent to pipeline for processing to database

--- freeweibo/freeweibo/spiders/items.py
+++ freeweibo/freeweibo/spiders/items.py
@@ -7,18 +7,6 @@ import scrapy
 
 
 class FreeweiboItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    # username = scrapy.Field()
-    # usernamelink = scrapy.Field()
-    # hashtags = scrapy.Field()
-    # hashtagslinks = scrapy.Field()
-    # content = scrapy.Field()
-    # date = scrapy.Field()
-    # censored = scrapy.Field()
-    # now = scrapy.Field()
-    #pass
-    
     username = scrapy.Field()
     postid = scrapy.Field()
     repostscount = scrapy.Field()
@@ -29,6 +17,5 @@ class FreeweiboItem(scrapy.Item):
     time_created = scrapy.Field()
     freeweiboOGpostlink = scrapy.Field()
     content = scrapy.Field()
-    hashtags = scrapy.Field()
-    hashtagsurls = scrapy.Field()
-    timestampscrapped = scrapy.Field()
+    hotterm = scrapy.Field()
+    timestampPostscrapped = scrapy.Field()
