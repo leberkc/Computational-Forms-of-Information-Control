1b217dc00040072776a72e38fda135e290db01a0
Chris Leberknight <32943348+leberkc@users.noreply.github.com>
2021-08-05 19:48:08 -0400

    Create create_processed.py

---
+++ censorship model/create_processed.py
@@ -0,0 +1,19 @@
+#encoding=utf-8
+import csv
+import jieba
+import pandas as pd
+import jieba.posseg as pseg
+
+
+df1= pd.read_csv('/home/leberkc/fastText/sandbox/freeweibo.labeled.csv', sep='\t')
+#df1= pd.read_csv('/home/leberkc/fastText/sandbox/freeweibo.labeled.csv', sep='\t', error_bad_lines=False)
+#df2= pd.read_csv('/home/leberkc/fastText/sandbox/freeweibo.segmented.csv', sep='\t')
+df2= pd.read_csv('/home/leberkc/fastText/sandbox/freeweibo.clean.content.csv', sep='\t')
+
+#print(df1)
+#print(df2)
+#print(df1['HotTerm'])
+#print(df2['cleaned_content'])
+
+df3 = pd.concat([df1['HotTerm'], df2['cleaned_content']], axis=1)
+df3.to_csv(r'/home/leberkc/fastText/sandbox/freeweibo.processed.csv', index=False, sep='\t', quoting=csv.QUOTE_NONE, quotechar="", escapechar=" ")
