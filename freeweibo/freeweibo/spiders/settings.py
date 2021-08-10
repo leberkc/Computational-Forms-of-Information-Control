7465af0cb19d86616edc921dbff479fbc8bc1c2a
ortizjhony <86069583+ortizjhony@users.noreply.github.com>
2021-08-09 21:03:22 -0400

    Update settings.py
    Edit Line 20 ROBOTSTXT_OBEY to False in order to scrap Weibo API

--- freeweibo/freeweibo/spiders/settings.py
+++ freeweibo/freeweibo/spiders/settings.py
@@ -17,7 +17,7 @@ FEED_EXPORT_ENCODING = 'utf-8'
 #USER_AGENT = 'freeweibo (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
