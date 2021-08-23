b9db93efb9e43d6ddd850db73a883831209b830b
ortizjhony <86069583+ortizjhony@users.noreply.github.com>
2021-08-23 08:32:30 -0400

    Update and rename freeweibo/Weibo_user_data.sql to Database/Weibo_user_data.sql
    Update FK's, add new fields and move file to new folder

--- Database/Weibo_user_data.sql
+++ Database/Weibo_user_data.sql
@@ -4,18 +4,30 @@ CREATE TABLE Weibo_User_data (
 weibo_user_id VARCHAR(20),
 screen_name VARCHAR(100),
 profile_url TINYTEXT,
+statuses_count INT,
+verified BOOLEAN,
+verified_type INT,
+verified_reason TEXT,
+close_blue_v BOOLEAN,
+u_description TEXT,
 gender char,
+mbtype INT,
+urank INT,
+mbrank INT,
+follow_me BOOLEAN,
+following BOOLEAN,
 followers_count INT,
 follow_count INT,
+origin VARCHAR(10),
 time_scrapped DATETIME,
 PRIMARY KEY(weibo_user_id, screen_name)
 )ENGINE = InnoDB
 
 ALTER TABLE `FreeWeibo`.`Weibo_User_data` 
-ADD CONSTRAINT `fk_user`
+ADD CONSTRAINT `fk_weibo_user`
   FOREIGN KEY (`weibo_user_id`)
-  REFERENCES `FreeWeibo`.`FreeWeiboPosts` (`weibo_id_user`)
-  ON DELETE CASCADE
+  REFERENCES `FreeWeibo`.`Weibo_HotTerm_Topic_Post` (`weibo_user_id`)
+  ON DELETE NO ACTION
   ON UPDATE NO ACTION;
-
+  
 GRANT ALL ON FreeWeibo.Weibo_User_data TO 'admin'@'localhost';
